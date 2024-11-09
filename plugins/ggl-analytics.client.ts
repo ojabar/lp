cf3ebc33d8215012ba92659c87738668883c6a0c
ojabar <jabar.omr@gmail.com>
2024-11-09 20:38:22 +0100

    Update ggl-analytics.client.ts

--- plugins/ggl-analytics.client.ts
+++ plugins/ggl-analytics.client.ts
@@ -3,7 +3,7 @@ import VueGtag, { trackRouter } from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'GTM-KCDM6TLV'
+      id: 'G-JKD9V372MK'
     }
   })
   trackRouter(useRouter())
